4d6643b8464dbcf1a0c3609483083a78999b4f60
Suraj <surajdasarwad1@gmail.com>
2022-02-21 18:05:41 +0530

    Docker file added

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*var count=34;
+count++;
+console.log("Server is running");
+console.log(count);
+
+
+//CTL + SHIFT + C   
+
+*/
+
 var express =require("express");  //lib-----Node Module
 var app=express();  // will create app object from express package
 
@@ -5,16 +15,24 @@ var onAboutUs=function(req, res){
     res.send("Chief Mentor :Ravi Tambade");
 };
 var onDefault=function(req, res){
-    res.send("<h1>Transflower Learning Pvt. Ltd</h1>"+
+    res.send("<h1>Fabrikam Pvt. Ltd  </h1>"+
               "<hr/>"+
               "<ol>"+
-                    " <li>Mentoring for skill building</li>"+
-                    " <li>Workshops</li>"+
-                    " <li>Online Course</li>"+
-                    " <li>Corporate Training</li>"+
+                    " <li>Fabrikam is a multinational company has their 24 Offices all over world. Company wanted to build and host Information Management Solutions in web. </li>"+
+                    " <li>Company also wants to host internet public web site for their potential customers to know about products and services provided</li>"+
+                    " <li>They required to build and host intranet application for all employees to access Human Resource, Accounting and Project Management related information.</li>"+
+                    " <li>Fabrikam has public Cloud subscription to use it as Infrastructure as a Service (I-a-a-S) and Platform As A Service.</li>"+
              "</ol>");
 };
 app.get("/",onDefault);  // Request handler functions are registered
 app.get("/aboutus",onAboutUs);  // Request handler functions are registered
 var server=app.listen(8081);
 console.log("Server is running on port 8081");
+// if external packages are required
+// we have to download those packages from internet using  package manager tool
+// Runtime--------tools-------------packages-----------------------config file
+// Java-----------maven--------------.jar files----------------------pom.xml
+// Android--------graddle------------android jar file----------------pom.xml
+// Python---------pip----------------Python libraries----------------
+// .NET ---------Nugget--------------dll files-----------------------package.config
+// NodeJS----------npm --------------download node modules-----------package.json 
